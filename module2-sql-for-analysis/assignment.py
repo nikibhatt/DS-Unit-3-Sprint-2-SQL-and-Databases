ff3eb71e3ad8cad21b883312d7a01b135ff1847b
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-10 16:16:41 -0800

    added comments

--- module2-sql-for-analysis/assignment.py
+++ module2-sql-for-analysis/assignment.py
@@ -1,19 +1,20 @@
 import pandas as pd
 import psycopg2
 
+# Neccessary information about the Database
 dbname = 'uxdlljnv'
 user = 'uxdlljnv'
 password = 'PrNBttEvyflonZQbwTptgWqxwZkLWgc6'
 host = 'rajje.db.elephantsql.com'
 
+#Make connection and get cursor
 pg_conn = psycopg2.connect(dbname=dbname, user=user, password=password, host=host)
 pg_curs = pg_conn.cursor()
 
 df = pd.read_csv('titanic.csv')
 
+#insert each row by iterating through the dataframe
 for index, row in df.iterrows():
-  # row['Name'] = row['Name'].astype(str)
-  # row['Sex'] = row['Sex'].astype(str)
   row_string = '(' + str(row['Survived']) + ', ' + str(row['Pclass']) + ', ' + "'" + row['Name'].replace("'", "") + "'" + ', ' + "'" + row['Sex'] + "'" + ', ' + str(row['Age']) + ',' + str(row["Siblings/Spouses Aboard"]) + ', ' + str(row["Parents/Children Aboard"]) + ', ' + str(row['Fare']) + ')'
   insert_row = """
     INSERT INTO titanic
@@ -21,5 +22,6 @@ for index, row in df.iterrows():
     VALUES """ + row_string + ";"
   pg_curs.execute(insert_row)
 
+#Close and Commit the changes to the database
 pg_curs.close()
 pg_conn.commit()
