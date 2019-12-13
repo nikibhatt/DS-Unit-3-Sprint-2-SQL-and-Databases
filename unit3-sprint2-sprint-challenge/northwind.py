28b94435f0e3f0e71edacdf8a2be4ad87827d204
nikibhatt <nkibhatt@users.noreply.github.com>
2019-12-13 10:21:24 -0800

    including part5

--- unit3-sprint2-sprint-challenge/northwind.py
+++ unit3-sprint2-sprint-challenge/northwind.py
@@ -5,15 +5,19 @@ import sqlite3
 conn = sqlite3.connect('northwind_small.sqlite3')
 
 curs = conn.cursor()
-query = 'select ProductName, CompanyName as Supplier from Product, Supplier where Product.SupplierId = Supplier.Id Order by UnitPrice desc limit 10'
 
-print(curs.execute(query).fetchall.__doc__)
-print('10 most expensive items and their suppliers', curs.execute(query).fetchall())
+def execute_with_doc_string(query, msg):
+    """Executing Query provided and printing with message provided"""
+    print(msg, curs.execute(query).fetchall())
+
+query = 'select ProductName, CompanyName as Supplier from Product, Supplier where Product.SupplierId = Supplier.Id Order by UnitPrice desc limit 10'
+msg = '10 most expensive items and their suppliers'
+execute_with_doc_string(query, msg)
 
 query = 'select Category.CategoryName, count(*) from Product,Category where Product.CategoryId = Category.Id group by Product.CategoryId order by count(*) DESC limit 1'
+msg = 'The largest category by number of unique products in it is: '
+execute_with_doc_string(query, msg)
 
-print('The largest category by number of unique products in it is: ', curs.execute(query).fetchone())
-
-query = 'select Employee.FirstName, Employee.LastName, count(EmployeeTerritory.TerritoryId) from Employee, EmployeeTerritory where Employee.Id = EmployeeTerritory.EmployeeId GROUP by EmployeeTerritory.EmployeeId order by count(EmployeeTerritory.TerritoryId) desc limit 1'
-
-print('The employee with the most territories is: ', curs.execute(query).fetchone())
+query = 'select Employee.FirstName || \' \' || Employee.LastName as FullName, count(EmployeeTerritory.TerritoryId) from Employee, EmployeeTerritory where Employee.Id = EmployeeTerritory.EmployeeId GROUP by EmployeeTerritory.EmployeeId order by count(EmployeeTerritory.TerritoryId) desc limit 1'
+msg = 'The employee with the most territories is: '
+execute_with_doc_string(query, msg)
